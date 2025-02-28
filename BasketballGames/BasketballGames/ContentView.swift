52417fd2c3a5f6488ec8cc5a44be477d7d71e936
Madison Roberts <mlr25@ad.unc.edu>
2025-02-28 14:09:10 -0500

    UNC Basketball Games API call

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,17 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+struct Result: Codable, Identifiable {
+    var team: String
+    var id: Int
+    var score: Score
+    var date: String
+    var opponent: String
+    var isHomeGame: Bool
+    
+    struct Score: Codable {
+        var opponent: Int
+        var unc: Int
     }
 }
+
+struct ContentView: View {
+    @State private var results = [Result]()
+
+    var body: some View {
+        NavigationView {
+            List(results) { item in
+                HStack(alignment: .center) {
+                    VStack(alignment: .leading) {
+                        Text("UNC vs \(item.opponent)")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundStyle(.blue)
+                        Text("\(item.team)")
+                            .foregroundStyle(.secondary)
+                        Text("Date: \(item.date)")
+                            .foregroundStyle(.secondary)
+                        Text("Location: \(item.isHomeGame ? "Home" : "Away")")
+                            .foregroundStyle(.secondary)
+                    }
+                    Spacer()
+                    Text("\(item.score.unc) - \(item.score.opponent)")
+                        .font(.title3)
+                }
+            }
+            .navigationTitle("UNC Basketball Games")
+        }
+        .task {
+            await loadData()
+        }
+    }
+
+    func loadData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            print("Invalid URL")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode([Result].self, from: data) {
+                DispatchQueue.main.async {
+                    results = decodedResponse
+                }
+            }
+        } catch {
+            print("Invalid data")
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
